b349dc53bf4ec071b889ad7490a94d45565803de
Alexander Shirk <alexander@truewheels.net>
2022-03-24 16:37:37 -0700

    added include guard in util.h

--- util.h
+++ util.h
@@ -1,3 +1,6 @@
+#ifndef UTIL_H
+#define UTIL_H
+
 #include "type.h"
 
 int get_block(int dev, int blk, char *buf);
@@ -19,3 +22,5 @@ int getino(char *pathname);
 int findmyname(MINODE *parent, u32 myino, char myname[ ]);
 
 int findino(MINODE *mip, u32 *myino); // myino = i# of . return i# of ..    //this is the get_myino(MINODE *mip, int * parent_ino) function from the textbook
+
+#endif
