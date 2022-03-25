3426da73835745e9d306a35391bc20aee9048c56
Alexander Shirk <alexander@truewheels.net>
2022-03-24 19:19:49 -0700

    refactored globals into their own c file and allows accessing them with a bunch of externs in a globals.h header file which has been added to all the files that need to access globals.

--- cd_ls_pwd.c
+++ cd_ls_pwd.c
@@ -1,4 +1,5 @@
 #include "util.h"
+#include "globals.h"
 #include <sys/types.h>
 #include <unistd.h>
 /************* cd_ls_pwd.c file **************/
--- main.c
+++ main.c
@@ -14,17 +14,7 @@
 
 #include "util.h"
 
-MINODE minode[NMINODE];
-MINODE *root;
-PROC   proc[NPROC], *running;
-
-char gpath[128]; // global for tokenized components
-char *name[64];  // assume at most 64 components in pathname
-int   n;         // number of component strings
-
-int fd, dev;
-int nblocks, ninodes, bmap, imap, iblk;
-char line[128], cmd[32], pathname[128];
+#include "globals.h"
 
 #include "cd_ls_pwd.c"
 
