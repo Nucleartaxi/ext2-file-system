dcd7fd511b4a43ba7e77a0f3dc229a9c3b1b2dc9
Alexander Shirk <alexander@truewheels.net>
2022-03-30 23:23:57 -0700

    mkdir steps 1-3 complete. mkdir gets the dirname and basename, checks that dirname is a dir, and checks that basename does not already exist in the parent DIR.

--- mkdir_creat.c
+++ mkdir_creat.c
@@ -1,11 +1,37 @@
-#include "type.h"
-#include "globals.h"
 #include <string.h>
 #include <libgen.h>
 #include <stdio.h>
 
+#include "type.h"
+#include "globals.h"
+#include "util.h"
+
+
 int my_mkdir() { 
-    char* dname = dirname(pathname);
-    char* bname = basename(pathname);
+    //divide pathname into dname and bname
+    char dname_buf[128];
+    char bname_buf[128];
+    strcpy(dname_buf, pathname);
+    char* dname = dirname(dname_buf);
+    strcpy(bname_buf, pathname);
+    char* bname = basename(bname_buf);
     printf("my_mkdir dname=%s bname=%s\n", dname, bname);
+
+    //dirname must exist and is a DIR
+    int pino = getino(dname);
+    MINODE* pmip = iget(dev, pino);
+    if ((pmip->INODE.i_mode & 0xF000) == 0x4000) { // if (S_ISDIR())
+        printf("%s is a dir\n", dname);
+    } else {
+        printf("Error: %s is not a dir\n", dname);
+        return -1;
+    }
+
+    //basename must not exist in parent DIR:
+    if (search(pmip, bname) == 0) {
+        printf("basename %s does not exist in parent directory. This is good\n", bname);
+    } else {
+        printf("Error: basename %s already exists in parent directory.\n", bname);
+        return -1;
+    }
 }
