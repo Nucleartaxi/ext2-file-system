3bf4749447520c8577fc098e1bcbf3915bf22e77
Alexander Shirk <alexander@truewheels.net>
2022-04-11 23:12:27 -0700

    open step 2 complete

--- open_close.c
+++ open_close.c
@@ -2,6 +2,10 @@
 #include "util.h"
 #include "mkdir_creat.h"
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+enum MODE {RD=0, WR=1, RW=2, APPEND=3};
 
 int my_open() {
     int ino = getino(pathname); //gets ino of pathname
@@ -13,6 +17,27 @@ int my_open() {
     }
     MINODE* mip = iget(dev, ino);
     printf("ino=%d\n", mip->ino);
+
+    //allocate an openTable entry OFT; initialize OFT entries 
+    for (int i = 0; i < NOFT; ++i) { //iterate through the oft array
+        if (oft[i].refCount==0) { //allocate a new oft
+            printf("oft[%d].refCount==0\n", i);
+            int mode = strtol(pathname2, NULL, 10);
+            printf("mode=%d\n", mode);
+
+            oft[i].mode = mode;
+            oft[i].minodePtr = mip;
+            oft[i].refCount = 1;
+            if (mode == APPEND) { //for append mode, set offset to file size
+                printf("mode=append\n");
+                oft[i].offset = mip->INODE.i_size;
+            } else {
+                printf("mode=read, write, or read-write\n");
+                oft[i].offset = 0;
+            }
+            break;
+        }
+    }
 }
 int my_close() {
 
