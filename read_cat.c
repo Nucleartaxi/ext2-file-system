76558e226f5acb628e88c26c82a4613882a94d88
Quickbick <nathanael.ostheller@wsu.edu>
2022-04-13 20:08:39 -0700

    something from yesterday, IDK

--- read_cat.c
+++ read_cat.c
@@ -1,38 +1,62 @@
 #include "read_cat.h"
 
+//called read fd_name number_bytes
 int read_file(){
-    //do later
+    //get fd from pathname
+    if(pathname2){
+        int nbytes = atoi(pathname2);
+    }
 }
 
-int myread(int fd, char *buf, int nbytes){
-    int count = 0;
+int myread(int fd, char *buf, int nbytes){ //needs to be optimized
+    int count = 0, blk = 0;
     MINODE *mip = proc[0].fd[fd]->minodePtr;
     int avil = mip->INODE.i_size - proc[0].fd[fd]->offset; //number of bytes still availible in file
     char readBuf[BLKSIZE];
+    char* cq = buf;
 
     while(nbytes && avil){
         int lbk = proc[0].fd[fd]->offset / BLKSIZE; //logical block
         int startByte = proc[0].fd[fd]->offset % BLKSIZE; //start read byte
 
         if(lbk < 12){ //direct block
-            int blk = mip->INODE.i_block[lbk];
+            blk = mip->INODE.i_block[lbk];
         }
         else if(lbk >= 12 && lbk < 256 + 12){ //indirect block
-            //do later
+            int ibuf[256];
+            get_block(fd, ip->i_block[12], ibuf);
+            blk = ibuf[lbk - 12];
         }
         else{ //double indirect block
-            //do later
+            int ibuf[256], ibuf2[256];
+            get_block(fd, ip->i_block[13], ibuf);
+            int lbkSet, lbkOffset;
+            lbkSet = (lbk - 268) / 256;
+            lbkOffset = (lbk - 268) % 256;
+            get_block(fd, ibuf[lbkSet], ibuf2);
+            blk = ibuf2[lbkOffset];
         }
 
         //get data block into readbuf
-        get_block(mip->dev, blk, readbuf);
+        get_block(mip->dev, blk, readBuf);
 
         //copy from startByte to buf, at most remain bytes in this block
         char* cp = readBuf + startByte;
-        //cont from here
+        int remain = BLKSIZE - startByte; //bytes remaining
+
+        while(remain > 0){
+            *cq++ = *cp++;
+            oft->offset++;
+            count++;
+            avil--;
+            nbytes--;
+            remain--;
+            if(nbytes <= 0 || avil <= 0){
+                break;
+            }
+        }
+        //loops back through while loop if one block is not enough
     }
-
-
-
-    //int remain = BLKSIZE - start; //bytes remaining
+    printf("myread: read %d char from file descriptor %d\n", count, fd);  
+    return count;   // count is the actual number of bytes read
 }
--- read_cat.h
+++ read_cat.h
@@ -1,6 +1,7 @@
 #ifndef READ_CAT_H
 #define READ_CAT_H
 
+#include <stdlib.h>
 #include "type.h"
 #include "globals.h"
 #include "util.h"
