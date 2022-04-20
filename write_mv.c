b32a1d126590a496b635e96e1121fca9f116e585
Alexander Shirk <alexander@truewheels.net>
2022-04-20 14:31:49 -0700

    started write with error checking

--- write_mv.c
+++ write_mv.c
@@ -5,5 +5,16 @@ int my_write(int fd, char* buf, int nbytes) {
 
 }
 int write_file() {
-
+    int fd = pathname_to_fd(pathname);
+    if (fd < 0) {
+        printf("Write error: no fd for pathname=%s\n", pathname);
+        return -1;
+    }
+    //fd exists 
+    if (proc[0].fd[fd]->mode != 0) { //opened for W, RW, or APPEND
+        printf("fd=%d is valid mode\n", fd);
+        
+    } else {
+        printf("Write error: fd=%d already opened for read so we cannot write\n", fd);
+    }
 }
