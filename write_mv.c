4eb7e9ec3272aa773cff9eba1a99404d7c9422ed
Alexander Shirk <alexander@truewheels.net>
2022-04-20 14:18:31 -0700

    added declarations for my_write and my_write_file

---
+++ write_mv.c
@@ -0,0 +1,9 @@
+#include "write_mv.h"
+
+
+int my_write(int fd, char* buf, int nbytes) {
+
+}
+int write_file() {
+
+}
---
+++ write_mv.h
@@ -0,0 +1,12 @@
+#ifndef WRITE_MV_H
+#define WRITE_MV_H
+
+#include "globals.h"
+#include "type.h"
+#include "string.h"
+
+int my_write(int fd, char* buf, int nbytes); 
+int write_file();
+
+
+#endif
